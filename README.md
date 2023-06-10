d730e1446d91631e1fd2578ffabf2b5e18a8c4d6
Nicolas Capparelli <CAPPARELLINICO@GMAIL.COM>
2023-06-10 11:15:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_application_nav_and_state
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_application_nav_and_state/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_nav_and_state
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
